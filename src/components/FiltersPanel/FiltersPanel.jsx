7b498a4fd127b1070b90833d120233393fbebb64
Savelev Andrey <needway@list.ru>
2025-11-01 10:15:16 +0300

    Expand filters panel module behavior and structure (2/4)

--- src/components/FiltersPanel/FiltersPanel.jsx
+++ src/components/FiltersPanel/FiltersPanel.jsx
@@ -31,3 +31,36 @@ function FiltersPanel({
           y: 0,
           duration: 0.28,
           ease: "power2.out",
+          stagger: 0.06,
+          delay: 0.06,
+        }
+      );
+    }, panelRef);
+
+    const fields = panelRef.current.querySelectorAll("select");
+    const listeners = [];
+
+    fields.forEach((element) => {
+      const onFocus = () => {
+        gsap.to(element, {
+          y: -1,
+          duration: 0.14,
+          ease: "power2.out",
+        });
+      };
+
+      const onBlur = () => {
+        gsap.to(element, {
+          y: 0,
+          duration: 0.14,
+          ease: "power2.out",
+        });
+      };
+
+      element.addEventListener("focus", onFocus);
+      element.addEventListener("blur", onBlur);
+      listeners.push([element, onFocus, onBlur]);
+    });
+
+    return () => {
+      listeners.forEach(([element, onFocus, onBlur]) => {
