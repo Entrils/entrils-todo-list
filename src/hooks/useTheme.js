364eb055b469d662e41981eaf70cc1afa2295ac3
Savelev Andrey <needway@list.ru>
2026-01-30 11:43:36 +0300

    Finalize theme switching hook

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+﻿import { useEffect, useState } from "react";
 import usePersistentState from "../hooks/usePersistentState.js";
 
 function useTheme({ themes, defaultTheme = "linen" }) {
@@ -14,3 +14,19 @@ function useTheme({ themes, defaultTheme = "linen" }) {
   useEffect(() => {
     setActiveTheme(themes.find((item) => item.id === theme) || themes[0]);
   }, [theme, themes]);
+
+  const handleThemeNext = () => {
+    const index = themes.findIndex((item) => item.id === theme);
+    const next = themes[(index + 1) % themes.length];
+    setTheme(next.id);
+  };
+
+  return {
+    theme,
+    setTheme,
+    activeTheme,
+    handleThemeNext,
+  };
+}
+
+export default useTheme;
